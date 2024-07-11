fc69fbb164ad9a33229f8986b0a44c86befd83f5
ATHER-101 <atharvatijare04@gmail.com>
2024-07-12 01:19:04 +0530

    using localStore

--- routes/authentication.mjs
+++ routes/authentication.mjs
@@ -8,39 +8,26 @@ const router = Router();
 router.get("/api/auth/google", passport.authenticate("google"));
 
 const generateAccessToken = (user) => {
-    return jwt.sign({ user }, 'YOUR_JWT_SECRET', { expiresIn: '1m' });
+    return jwt.sign({ user }, 'YOUR_JWT_SECRET', { expiresIn: '30s' });
 };
 
 const generateRefreshToken = (user) => {
-    return jwt.sign({ user }, 'YOUR_REFRESH_TOKEN_SECRET', { expiresIn: '3m' });
+    return jwt.sign({ user }, 'YOUR_REFRESH_TOKEN_SECRET', { expiresIn: '2m' });
 };
 
 router.get("/api/auth/google/callback",
-    passport.authenticate('google', { session: false }),
+    passport.authenticate('google', { session: false, failureRedirect:'/api/auth/failed' }),
     (req, res) => {
         const accessToken = generateAccessToken(req.user);
         const refreshToken = generateRefreshToken(req.user);
 
-        // Send the tokens in cookies
-        res.cookie('accessToken', accessToken, {
-            httpOnly: false, // Not setting httpOnly for less security
-            secure: false, // Not setting Secure for less security
-            sameSite: 'none' // You can adjust this attribute as needed
-        });
-
-        res.cookie('refreshToken', refreshToken, {
-            httpOnly: false, // Not setting httpOnly for less security
-            secure: false, // Not setting Secure for less security
-            sameSite: 'none' // You can adjust this attribute as needed
-        });
-
-        res.redirect(`${process.env.FRONTEND}`);
+        res.redirect(`${process.env.FRONTEND}?accessToken=${accessToken}&refreshToken=${refreshToken}`);
     }
 );
 
 
 router.post('/api/auth/token', (req, res) => {
-    const { refreshToken } = req.cookies;
+    const { refreshToken } = req.body;
     if (!refreshToken) {
         return res.status(401).send('Access Denied');
     }
@@ -48,11 +35,6 @@ router.post('/api/auth/token', (req, res) => {
     try {
         const decoded = jwt.verify(refreshToken, 'YOUR_REFRESH_TOKEN_SECRET');
         const accessToken = generateAccessToken(decoded.user);
-        res.cookie('accessToken', accessToken, {
-            httpOnly: false, // Not setting httpOnly for less security
-            secure: false, // Not setting Secure for less security
-            sameSite: 'none' // You can adjust this attribute as needed
-        });
         res.send({ accessToken });
     } catch (error) {
         res.status(403).send('Invalid Refresh Token');
@@ -60,13 +42,13 @@ router.post('/api/auth/token', (req, res) => {
 });
 
 const verifyToken = (req, res, next) => {
-    const token = req.cookies.accessToken;
-    if (!token) {
+    const {accessToken} = req.query;
+    if (!accessToken) {
         return res.status(401).send('Access Denied');
     }
 
     try {
-        const decoded = jwt.verify(token, 'YOUR_JWT_SECRET');
+        const decoded = jwt.verify(accessToken, 'YOUR_JWT_SECRET');
         req.user = decoded.user;
         next();
     } catch (error) {
@@ -75,7 +57,6 @@ const verifyToken = (req, res, next) => {
 };
 
 router.get("/api/auth/status", verifyToken, (request, response) => {
-    console.log(request.headers.cookie); // Log cookie headers
     if (request.user) {
         if (request.user.email === 'atharvatijare04@gmail.com') {
             request.user.role = 'admin';
@@ -91,18 +72,6 @@ router.get("/api/auth/status", verifyToken, (request, response) => {
 });
 
 router.get("/api/auth/logout", (request, response) => {
-    response.clearCookie('accessToken', {
-        httpOnly: false, // Not setting httpOnly for less security
-        secure: false, // Not setting Secure for less security
-        sameSite: 'none' // You can adjust this attribute as needed
-    });
-
-    response.clearCookie('refreshToken', {
-        httpOnly: false, // Not setting httpOnly for less security
-        secure: false, // Not setting Secure for less security
-        sameSite: 'none' // You can adjust this attribute as needed
-    });
-
     response.redirect(process.env.FRONTEND);
 });
 
