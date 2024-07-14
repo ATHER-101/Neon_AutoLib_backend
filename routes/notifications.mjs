72e4f03215388fea2e0e8387e92dca123eee797f
ATHER-101 <atharvatijare04@gmail.com>
2024-07-14 22:52:32 +0530

    notification delete

--- routes/notifications.mjs
+++ routes/notifications.mjs
@@ -3,13 +3,32 @@ import pool from "../db.mjs";
 
 const router = Router();
 
-// get user
+// get notifications
 router.get("/api/notifications", async (request, response) => {
     const { user_id } = request.query;
     const client = await pool.connect();
     try {
         const res = await client.query(`SELECT * FROM notifications WHERE user_id=$1;`, [user_id]);
         return response.status(200).send(res.rows);
+    } catch (error) {
+        return response.status(500).send({ error: error.message });
+    } finally {
+        client.release();
+    }
+})
+
+// delete notification
+router.delete("/api/notifications", async (request, response) => {
+    const { user_id } = request.body;
+    const { title } = request.body;
+    const { description } = request.body;
+
+    console.log({user_id,title,description});
+
+    const client = await pool.connect();
+    try {
+        await client.query(`DELETE FROM notifications WHERE user_id=$1 AND title=$2 AND description=$3;`, [user_id,title,description]);
+        return response.sendStatus(200);
     } catch (error) {
         return response.status(500).send({ error: error.message });
     } finally {
