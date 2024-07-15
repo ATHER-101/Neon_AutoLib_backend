c229188995072e74b4ee2e05deabc805f08c6714
ATHER-101 <atharvatijare04@gmail.com>
2024-07-15 18:54:02 +0530

    secured cron job

--- routes/cron-jobs.mjs
+++ routes/cron-jobs.mjs
@@ -3,19 +3,24 @@ import sendReminderMail from "../cron-jobs/reminder-mail.mjs";
 
 const router = Router();
 
-router.get('/api/send-mail', async (request, response) => {
-    try {
-        const res = await sendReminderMail();
+router.post('/api/send-mail', async (request, response) => {
+    const { user, password } = request.body;
+    if (user === process.env.CRON_JOB_USER && password === process.env.CRON_JOB_PASSWORD) {
+        try {
+            const res = await sendReminderMail();
 
-        const errors = res.filter(result => result?.error);
-        if (errors.length > 0) {
-            response.status(500).send({ message: 'Some emails failed to send.', errors });
-        } else {
-            response.status(200).send('Mails sent successfully!');
+            const errors = res.filter(result => result?.error);
+            if (errors.length > 0) {
+                response.status(500).send({ message: 'Some emails failed to send.', errors });
+            } else {
+                response.status(200).send('Mails sent successfully!');
+            }
+        } catch (error) {
+            console.error("Error in /api/send-mail route:", error);
+            response.status(500).send({ message: 'An error occurred while sending emails.', error });
         }
-    } catch (error) {
-        console.error("Error in /api/send-mail route:", error);
-        response.status(500).send({ message: 'An error occurred while sending emails.', error });
+    }else{
+        response.status(404).send({message:"Not Authorised!"})
     }
 });
 
